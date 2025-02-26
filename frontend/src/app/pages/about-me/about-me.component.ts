475da137cfa25d0f02c8107e2440309b5e03cb42
TrueGeorgi <110535908+TrueGeorgi@users.noreply.github.com>
2025-02-26 11:00:29 +0100

    added About-me page UI
    * Added the about-me page UI

--- frontend/src/app/pages/about-me/about-me.component.ts
+++ frontend/src/app/pages/about-me/about-me.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
-
+  email: string = 'georgi.iliev9191@gmail.com'
+  text: string = ' Hi! I\'m Georgi, a passionate software developer with a love for creating intuitive and engaging applications. I enjoy watching and playing football, which inspired me to create this Goleador app, inspired by different football manager games that I\'m playing. Beyond coding, I love experimenting in the kitchen and diving into captivating audiobooks.'
 }
