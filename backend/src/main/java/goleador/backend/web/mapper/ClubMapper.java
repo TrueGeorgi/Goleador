b003fdd63c7c4044dea888307bca5d9da1683c26
TrueGeorgi <110535908+TrueGeorgi@users.noreply.github.com>
2025-03-19 12:32:28 +0100

    Club Mapper
    Club mapper created

---
+++ backend/src/main/java/goleador/backend/web/mapper/ClubMapper.java
@@ -0,0 +1,29 @@
+package goleador.backend.web.mapper;
+
+
+import goleador.backend.domain.club.model.Club;
+import goleador.backend.domain.club.model.Game;
+import goleador.backend.domain.player.model.Player;
+import goleador.backend.web.dto.ClubData;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class ClubMapper {
+
+    public ClubData toClubData(Club club) {
+        return ClubData.builder()
+                .clubName(club.getName())
+                .createdOn(club.getCreatedOn())
+                .managerId(club.getManager().getId())
+                .logo(club.getLogo())
+                .playersIds(club.getPlayers().stream().map(Player::getId).collect(Collectors.toList()))
+                .homeGamesIds(club.getHomeGames().stream().map(Game::getId).collect(Collectors.toList()))
+                .awayGamesIds(club.getAwayGames().stream().map(Game::getId).collect(Collectors.toList()))
+                .points(club.getPoints())
+                .finances(club.getFinances())
+                .build();
+    }
+
+}
